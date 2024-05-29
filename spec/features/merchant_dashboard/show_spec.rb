e6d17676e4ab2192143ad1a61297b1be8254d871
James McHugh <jdmchugh111@gmail.com>
2024-05-29 16:47:01 -0400

    Wrote and passed test for User Story 2

--- spec/features/merchant_dashboard/show_spec.rb
+++ spec/features/merchant_dashboard/show_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe "the merchant dashboard page" do
 
     expect(page).to have_content("Nordstrom")
   end
+
+  it "has links for merchant items index and merchant invoices index" do
+    @merchant1 = Merchant.create!(name: "Nordstrom")
+
+    visit "/merchants/#{@merchant1.id}/dashboard"
+
+    expect(page).to have_link("Items Index")
+    expect(page).to have_link("Invoices Index")
+  end
 end
