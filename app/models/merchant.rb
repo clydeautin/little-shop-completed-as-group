6f407887f2b447e8ff976a49dca1fda47f14137d
Chee Lee <cheeleertr@gmail.com>
2024-06-01 15:36:44 -0500

    us3 add #top_five_customers, #successful_transactions_with_merchant methods, update associations, refactor

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,9 +1,25 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
-  
+
   has_many :items
   has_many :invoice_items, through: :items
   has_many :invoices, through: :items
   has_many :transactions, through: :items
   has_many :customers, through: :invoices
+
+  def top_five_customers
+    @transaction_ids = transactions.pluck(:id)
+
+    self.customers.joins(:transactions)
+      .where(transactions: {result: 'success', id: @transaction_ids})
+      .group('customers.id')
+      .order('count(transactions.id) desc')
+      .limit(5)
+  end
+
+  #   customers.joins(:transactions).where("transactions.result = ? and customers.id = ?", "success", @customer_ids)
+  #   # Customer.joins(invoices: :transactions).where(invoices: {customer_id: @ids}, transactions: {result: 'success'}).select("customers.*, transactions.id, invoices.customer_id")
+  #   Customer.joins(invoices: :transactions).where(invoices: {customer_id: @customer_ids}, transactions: {result: 'success'}).distinct
+  #   Customer.joins(invoices: :transactions).where(invoices: {customer_id: @customer_ids}, transactions: {result: 'success'}).select('customers.*, COUNT(transactions.id) AS transactions_count')
+
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,4 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
+  it {should validate_presence_of :first_name}
+  it {should validate_presence_of :last_name}
+  
+  it {should have_many :items}
+  it {should have_many(:invoice_items).through(:invoices)}
+  it {should have_many(:items).through(:invoices)}
+  it {should have_many(:merchants).through(:invoices)}
+  it {should have_many(:transactions).through(:invoices)}
+
+  before :each do
+    @merchant = create(:merchant)
+    @merchant2 = create(:merchant)
+
+    @customer1 = create(:customer)
+    @customer2 = create(:customer)
+    @customer3 = create(:customer)
+    @customer4 = create(:customer)
+    @customer5 = create(:customer)
+    @customer6 = create(:customer)
+    @customer7 = create(:customer)
+
+    @item1 = create(:item, merchant: @merchant, unit_price: 1000)
+    @item2 = create(:item, merchant: @merchant, unit_price: 2000)
+    @item3 = create(:item, merchant: @merchant, unit_price: 3000)
+    @item4 = create(:item, merchant: @merchant, unit_price: 4000)
+    @item5 = create(:item, merchant: @merchant, unit_price: 5000)
+    @item6 = create(:item, merchant: @merchant, unit_price: 6000)
+    @item7 = create(:item, merchant: @merchant, unit_price: 7000)
+
+    @invoice1 = create(:invoice, customer: @customer1, status: 1)
+    @invoice_item1 = create(:invoice_item, invoice: @invoice1, item: @item1, quantity: 1, unit_price: @item1.unit_price)
+    6.times { create(:transaction, invoice: @invoice1, result: 'success') }
+
+    @invoice2 = create(:invoice, customer: @customer2, status: 1)
+    @invoice_item2 = create(:invoice_item, invoice: @invoice2, item: @item2, quantity: 1, unit_price: @item2.unit_price)
+    2.times { create(:transaction, invoice: @invoice2, result: 'success') }
+
+    @invoice3 = create(:invoice, customer: @customer3, status: 1)
+    @invoice_item3 = create(:invoice_item, invoice: @invoice3, item: @item3, quantity: 1, unit_price: @item3.unit_price)
+    4.times { create(:transaction, invoice: @invoice3, result: 'success') }
+
+    @invoice4 = create(:invoice, customer: @customer4, status: 1)
+    @invoice_item4 = create(:invoice_item, invoice: @invoice4, item: @item4, quantity: 1, unit_price: @item4.unit_price)
+    5.times { create(:transaction, invoice: @invoice4, result: 'success') }
+
+    @invoice5 = create(:invoice, customer: @customer5, status: 1)
+    @invoice_item5 = create(:invoice_item, invoice: @invoice5, item: @item1, quantity: 1, unit_price: @item1.unit_price)
+    3.times { create(:transaction, invoice: @invoice5, result: 'success') }
+
+    @invoice6 = create(:invoice, customer: @customer6, status: 0)
+    @invoice_item6 = create(:invoice_item, invoice: @invoice6, item: @item2, quantity: 1, unit_price: @item2.unit_price)
+    create(:transaction, invoice: @invoice6, result: 'success')
+
+    @invoice7 = create(:invoice, customer: @customer7, status: 0)
+    @invoice_item7 = create(:invoice_item, invoice: @invoice7, item: @item3, quantity: 1, unit_price: @item3.unit_price)
+    create(:transaction, invoice: @invoice7, result: 'success')
+  end
+
+  describe "instance methods" do
+    describe "#successful_transactions_with_merchant" do
+      it "can return count of successful transactions with a merchant" do
+        @merchant2 = create(:merchant)
+        @item8 = create(:item, merchant: @merchant2, unit_price: 7000)
+
+        @invoice8 = create(:invoice, customer: @customer1, status: 1)
+        @invoice_item8 = create(:invoice_item, invoice: @invoice8, item: @item8, quantity: 1, unit_price: @item1.unit_price)
+        8.times { create(:transaction, invoice: @invoice8, result: 'success') }
+
+        expect(@customer1.successful_transactions_with_merchant(@merchant)).to eq(6)
+        expect(@customer2.successful_transactions_with_merchant(@merchant)).to eq(2)
+      end
+    end
+  end
 end
