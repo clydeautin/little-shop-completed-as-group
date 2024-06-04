e1616d46481098dbd5f4b0abdfcba6cb8d273084
Clyde <clyde.autin@gmail.com>
2024-06-03 18:55:58 -0700

    fixes the invoice spec model

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -108,9 +108,9 @@ RSpec.describe Invoice, type: :model do
       @invoice12 = FactoryBot.create(:invoice, customer: @customer3, status: 1),
       @invoice13 = FactoryBot.create(:invoice, customer: @customer3, status: 1)
     ]
-    @invoice14 = FactoryBot.create(:invoice, customer: @customer4, status: 0)
-    @invoice15 = FactoryBot.create(:invoice, customer: @customer4, status: 0)
-    @invoice16 = FactoryBot.create(:invoice, customer: @customer4, status: 0)
+    @invoice14 = FactoryBot.create(:invoice, customer: @customer4, status: 1)
+    @invoice15 = FactoryBot.create(:invoice, customer: @customer4, status: 1)
+    @invoice16 = FactoryBot.create(:invoice, customer: @customer4, status: 2)
 
     @invoice17 = FactoryBot.create(:invoice, customer: @customer5, status: 1)
     @invoice18 = FactoryBot.create(:invoice, customer: @customer5, status: 1)
@@ -121,7 +121,7 @@ RSpec.describe Invoice, type: :model do
     # @invoice21 = FactoryBot.create(:invoice, customer: @customer6, status: 2)
     # @invoice22 = FactoryBot.create(:invoice, customer: @customer6, status: 2)
 
-    @invoice20 = FactoryBot.create(:invoice, customer: @customer7, status: 0)
+    @invoice20 = FactoryBot.create(:invoice, customer: @customer7, status: 1)
     # @invoice24 = FactoryBot.create(:invoice, customer: @customer7, status: 1)
     # @invoice25 = FactoryBot.create(:invoice, customer: @customer7, status: 2)
 
@@ -215,11 +215,68 @@ RSpec.describe Invoice, type: :model do
       expect(@invoice7.total_revenue).to eq(12900)
     end
 
-    describe "#incomplete" do
-      it "returns invoices that have items not shipped" do
+    it "returns invoices that have items not shipped" do
+      InvoiceItem.destroy_all
+      Transaction.destroy_all
+      Invoice.destroy_all
+      Customer.destroy_all
+      Item.destroy_all
+      Merchant.destroy_all
+      @merchant = create(:merchant)
 
-        expect(Invoice.incomplete).to eq([@invoice1, @invoice2, @invoice7, @invoice8])
-      end
+      @customer1 = create(:customer)
+      @customer2 = create(:customer)
+      @customer3 = create(:customer)
+      @customer4 = create(:customer)
+      @customer5 = create(:customer)
+      @customer6 = create(:customer)
+      @customer7 = create(:customer)
+
+      @item1 = create(:item, merchant: @merchant, unit_price: 1000)
+      @item2 = create(:item, merchant: @merchant, unit_price: 2000)
+      @item3 = create(:item, merchant: @merchant, unit_price: 3000)
+      @item4 = create(:item, merchant: @merchant, unit_price: 4000)
+      @item5 = create(:item, merchant: @merchant, unit_price: 5000)
+      @item6 = create(:item, merchant: @merchant, unit_price: 6000)
+      @item7 = create(:item, merchant: @merchant, unit_price: 7000)
+
+      @invoice1 = create(:invoice, customer: @customer1, status: 1)
+      @invoice_item1 = create(:invoice_item, invoice: @invoice1, item: @item1, quantity: 1, unit_price: @item1.unit_price, status: 1)
+      6.times { create(:transaction, invoice: @invoice1, result: 'success') }
+
+      @invoice2 = create(:invoice, customer: @customer2, status: 1)
+      @invoice_item2 = create(:invoice_item, invoice: @invoice2, item: @item2, quantity: 1, unit_price: @item2.unit_price, status: 1)
+      2.times { create(:transaction, invoice: @invoice2, result: 'success') }
+
+      @invoice3 = create(:invoice, customer: @customer3, status: 1)
+      @invoice_item3 = create(:invoice_item, invoice: @invoice3, item: @item3, quantity: 1, unit_price: @item3.unit_price, status: 2)
+      4.times { create(:transaction, invoice: @invoice3, result: 'success') }
+
+      @invoice4 = create(:invoice, customer: @customer4, status: 1)
+      @invoice_item4 = create(:invoice_item, invoice: @invoice4, item: @item4, quantity: 1, unit_price: @item4.unit_price, status: 2)
+      5.times { create(:transaction, invoice: @invoice4, result: 'success') }
+
+      @invoice5 = create(:invoice, customer: @customer5, status: 1)
+      @invoice_item5 = create(:invoice_item, invoice: @invoice5, item: @item1, quantity: 1, unit_price: @item1.unit_price, status: 2)
+      3.times { create(:transaction, invoice: @invoice5, result: 'success') }
+
+      @invoice6 = create(:invoice, customer: @customer6, status: 0)
+      @invoice_item6 = create(:invoice_item, invoice: @invoice6, item: @item2, quantity: 1, unit_price: @item2.unit_price, status: 2)
+      create(:transaction, invoice: @invoice6, result: 'success')
+
+      @invoice7 = create(:invoice, customer: @customer7, status: 0)
+      @invoice_item7 = create(:invoice_item, invoice: @invoice7, item: @item3, quantity: 1, unit_price: @item3.unit_price, status: 0)
+      create(:transaction, invoice: @invoice7, result: 'success')
+
+      #for false positives
+      @merchant2 = create(:merchant)
+      @item8 = create(:item, merchant: @merchant2, unit_price: 7000)
+    
+      @invoice8 = create(:invoice, customer: @customer7, status: 1)
+      @invoice_item8 = create(:invoice_item, invoice: @invoice8, item: @item8, quantity: 1, unit_price: @item8.unit_price, status: 1)
+      8.times { create(:transaction, invoice: @invoice8, result: 'success') }
+
+      expect(Invoice.incomplete).to eq([@invoice1, @invoice2, @invoice7, @invoice8])
     end
   end
 end
